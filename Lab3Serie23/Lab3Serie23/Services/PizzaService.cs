30f9f0a02d085d3cbfb9733e817599799e217039
Allan Davila <allan.davila@hotmail.com>
2019-11-11 20:59:46 -0600

    Added MongoDB libraries and empty PizzaService.cs

---
+++ Lab3Serie23/Lab3Serie23/Services/PizzaService.cs
@@ -0,0 +1,13 @@
+﻿using Lab3Serie23.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Serie23.Services
+{
+    public class PizzaService
+    {
+    }
+}
