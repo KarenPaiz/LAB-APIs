7300fea798e120120e954ec384aab120f55edff4
Allan Davila <allan.davila@hotmail.com>
2019-11-11 21:27:47 -0600

    Modified startup, Serie2 y Serie3 finished

--- Lab3Serie23/Lab3Serie23/Startup.cs
+++ Lab3Serie23/Lab3Serie23/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Lab3Serie23.Models;
 using Lab3Serie23.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Lab3Serie23
