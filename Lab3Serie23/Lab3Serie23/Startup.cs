4045e9c2cc61f3483db24271dec854621da0b313
Allan Davila <allan.davila@hotmail.com>
2019-11-11 20:51:13 -0600

    Added Libraries to Startup.cs

--- Lab3Serie23/Lab3Serie23/Startup.cs
+++ Lab3Serie23/Lab3Serie23/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab3Serie23.Models;
+using Lab3Serie23.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Lab3Serie23
 {
