d297536f8fdf6459c7943660468d7b33da3517b5
Karenina <karenizabel1999@gmail.com>
2019-11-12 20:43:46 -0600

    Creacion proyecto serie I y clase Estudiante

---
+++ LAB-APIs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace LAB_APIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+    public class Estudiante
+    {
+        int carnet { get; set; }
+        string nombre { get; set; }
+        int edad { get; set; }
+        string carrera { get; set; }
+
+    }
+    
+
+   
+}
+
