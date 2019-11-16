99a6bed4fcd530648c29b0485dafcfd272f82cc0
Karenina <karenizabel1999@gmail.com>
2019-11-16 10:50:12 -0600

    Cambios chidos

--- LAB-APIs/Controllers/ValuesController.cs
+++ LAB-APIs/Controllers/ValuesController.cs
@@ -32,34 +32,26 @@ namespace LAB_APIs.Controllers
 
         // POST api/values
         [HttpPost("{value}")]
-        public void Post(string value, Estudiante elemento)
+        public string Post([FromBody]Dictionary<string,object>objeto, string value)
         {
-            var valueAux = value;
-            if (valueAux.Length < 32)
-            {
-                for (int i = 32; i > value.Length; i--)
-                {
-                    valueAux += ".";
-                }
-            }           
-            if (valueAux.Length > 32)
-            {
-                valueAux = valueAux.Substring(0, 32);
-            }
 
-            var password = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(valueAux));
-            var Credenciales = new SigningCredentials(password, SecurityAlgorithms.HmacSha256);
-            var peticiones = new Claim[4];//TAMANIO DE PROPIEDADES DE MI CLASE KAREN
-            peticiones[0] = new Claim("carnet", elemento.carnet.ToString());
-            peticiones[1] = new Claim("edad", elemento.edad.ToString());
-            peticiones[2] = new Claim("carrera", elemento.carrera);
-            peticiones[3] = new Claim("nombre", elemento.nombre);
-            var encabezado = new JwtHeader(Credenciales);
-            var cuerpo = new JwtPayload(peticiones);
-            var token = new JwtSecurityToken(encabezado, cuerpo);
-            //IdentityModelEventSource.showII = true;
-            var tokenstring = new JwtSecurityTokenHandler().WriteToken(token);
-            Singleton.Datos.token = tokenstring;
+            var buffer = value.PadRight(64, ' ')
+                .ToCharArray()
+                .Select(x => Convert.ToByte(x))
+                .ToArray();
+            var hander = new JwtSecurityTokenHandler();
+            var claims = objeto.Select(x => new Claim(x.Key, x.Value.ToString()));
+            var description = new SecurityTokenDescriptor {
+                Issuer = "todos",
+                Audience = "audiencia",
+                Expires = DateTime.UtcNow.AddSeconds(10),
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(buffer),SecurityAlgorithms.HmacSha256)
+            };
+            var token = hander.CreateToken(description);
+            
+            var tokenstring = hander.WriteToken(token);
+            return tokenstring;
         }
 
         // PUT api/values/5
@@ -70,8 +62,8 @@ namespace LAB_APIs.Controllers
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        public void Delete(int id) { 
+        
         }
         class Singleton
         {
